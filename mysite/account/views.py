b5f3df28dab205794c88a96dd02e153c3fc31dac
Emad <sudani.emad@gmail.com>
2024-03-05 21:26:57 +0330

    feat: add edit profile

--- mysite/account/views.py
+++ mysite/account/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm, UserRegisterationForm
+from .forms import LoginForm, UserRegisterationForm, UserEditForm, ProfileEditForm
 from .models import Profile
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def user_login(request):
@@ -46,3 +47,25 @@ def register(request):
         return render(request,
                         'account/register.html', 
                         { 'user_form': user_form })
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile,
+                                       data=request.POST)
+        
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    
+
+    return render(request, 'account/edit.html', {
+        'user_form': user_form,
+        'profile_form': profile_form,
+    })
